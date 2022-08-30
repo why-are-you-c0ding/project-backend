1e0d41279a9ee0f16e3ed2416ee9cabd02a8b0ad
happysubin <asb0711@gmail.com>
2022-08-30 18:35:56 +0900

    Feat: Spring Rest Docs 기초 설정

--- src/test/java/wayc/backend/unit/ControllerTest.java
+++ src/test/java/wayc/backend/unit/ControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -25,7 +26,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
+@AutoConfigureRestDocs
 public class ControllerTest extends MemberControllerTest {
 
     @Test
