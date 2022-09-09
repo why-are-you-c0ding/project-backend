c9a9462e8a9f555c57a987de754e90fb32aa6a85
happysubin <asb0711@gmail.com>
2022-09-09 23:28:55 +0900

    Fix: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ APP_NAME=action_codedeploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -fla java )
 
 if [ -z $CURRENT_PID ]
 then
