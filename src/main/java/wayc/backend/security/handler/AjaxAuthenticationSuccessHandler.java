a46c84dd48200218a2c23c8aeb9c5f82d8cc2699
happysubin <asb0711@gmail.com>
2022-09-10 19:24:25 +0900

    Fix: SameSite 오류 해결

--- src/main/java/wayc/backend/security/handler/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/wayc/backend/security/handler/AjaxAuthenticationSuccessHandler.java
@@ -10,6 +10,7 @@ import wayc.backend.security.dto.response.LoginResponseDto;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
