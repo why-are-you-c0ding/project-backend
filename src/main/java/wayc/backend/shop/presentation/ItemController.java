485deb02fff6f7adacffcf36c431b2c813ce5f14
happysubin <asb0711@gmail.com>
2022-09-17 16:05:40 +0900

    Test: 테스트

--- src/main/java/wayc/backend/shop/presentation/ItemController.java
+++ src/main/java/wayc/backend/shop/presentation/ItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/items")
 public class ItemController {
 
-    @GetMapping()
+    @PostMapping
     public void test(Authentication authentication, HttpServletRequest request){
         System.out.println("request = " + request);
         System.out.println("authentication = " + authentication.getName());
