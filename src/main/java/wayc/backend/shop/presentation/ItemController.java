247bcb1fc012281877cd6bc6fb976b1f87d715dc
happysubin <asb0711@gmail.com>
2022-09-17 15:27:34 +0900

    Feat: 검증을 위한 컨버터 메서드 추가

--- src/main/java/wayc/backend/shop/presentation/ItemController.java
+++ src/main/java/wayc/backend/shop/presentation/ItemController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/items")
 public class ItemController {
 
     @GetMapping()
-    public void test(Authentication authentication){
+    public void test(Authentication authentication, HttpServletRequest request){
+        System.out.println("request = " + request);
         System.out.println("authentication = " + authentication.getName());
     }
 }
