0f183a586109e9ab6b945034ce991ae14ece0965
happysubin <asb0711@gmail.com>
2022-09-27 13:05:01 +0900

    Fix: 테스트 값 수정

--- src/test/java/wayc/backend/factory/Item/PostItemRequestDtoFactory.java
+++ src/test/java/wayc/backend/factory/Item/PostItemRequestDtoFactory.java
@@ -10,13 +10,13 @@ public class PostItemRequestDtoFactory {
 
     public static PostItemRequestDto createSuccessCase(){
 
-        PostOptionRequestDto option1 = new PostOptionRequestDto("option1", 1000);
-        PostOptionRequestDto option2 = new PostOptionRequestDto("option2", 2000);
-        PostOptionRequestDto option3 = new PostOptionRequestDto("option3", 3000);
-        PostOptionRequestDto option4 = new PostOptionRequestDto("option4", 4000);
+        PostOptionRequestDto option1 = new PostOptionRequestDto("white", 1000);
+        PostOptionRequestDto option2 = new PostOptionRequestDto("black", 2000);
+        PostOptionRequestDto option3 = new PostOptionRequestDto("small", 3000);
+        PostOptionRequestDto option4 = new PostOptionRequestDto("large", 4000);
 
-        PostOptionGroupRequestDto dto1 = new PostOptionGroupRequestDto(Arrays.asList(option1, option2), "optionName1", true);
-        PostOptionGroupRequestDto dto2 = new PostOptionGroupRequestDto(Arrays.asList(option3, option4), "optionName2", false);
+        PostOptionGroupRequestDto dto1 = new PostOptionGroupRequestDto(Arrays.asList(option1, option2), "color", true);
+        PostOptionGroupRequestDto dto2 = new PostOptionGroupRequestDto(Arrays.asList(option3, option4), "sizee", false);
 
         return new PostItemRequestDto("item1",Arrays.asList(dto1, dto2));
     }
