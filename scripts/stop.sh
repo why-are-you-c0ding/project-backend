e4158c1895e5645f6b6a58eccbe3abc52bc02ebb
happysubin <asb0711@gmail.com>
2023-09-07 12:30:41 +0900

    chore: 블루 그린 배포 스크립트 작성

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,38 +1,18 @@
 #!/usr/bin/env bash
 
-# bash는 return value가 안되니 *제일 마지막줄에 echo로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
-# 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
-function find_idle_profile()
-{
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+source ./profile.sh
 
-    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
-    then
-        CURRENT_PROFILE=real2
-    else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
-    fi
+IDLE_PORT=$(find_idle_port)
 
-    if [ ${CURRENT_PROFILE} == real1 ]
-    then
-      IDLE_PROFILE=real2
-    else
-      IDLE_PROFILE=real1
-    fi
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
-    echo "${IDLE_PROFILE}"
-}
-
-# 쉬고 있는 profile의 port 찾기
-function find_idle_port()
-{
-    IDLE_PROFILE=$(find_idle_profile)
-
-    if [ ${IDLE_PROFILE} == real1 ]
-    then
-      echo "8081"
-    else
-      echo "8082"
-    fi
-}
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
---
+++ scripts/switch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source ./profile.sh
+
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
+
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+    echo "> 엔진엑스 Reload"
+    sudo service nginx reload
+}
