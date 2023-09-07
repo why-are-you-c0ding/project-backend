3589f370e59c1dff49d4d16511de06db487f6b92
happysubin <asb0711@gmail.com>
2023-09-07 12:54:52 +0900

    fix: 빌드 스크립트 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,4 +14,4 @@ echo "> $JAR_NAME 실행"
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_PATH  > $REPOSITORY/nohup.out 2>&1 &
+sudo nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_PATH  > $REPOSITORY/nohup.out 2>&1 &
