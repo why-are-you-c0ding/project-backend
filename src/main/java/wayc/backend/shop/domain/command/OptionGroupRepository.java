4f1fd9237158bc038774bb46b1d8743722baea01
happysubin <asb0711@gmail.com>
2023-12-13 17:08:17 +0900

    fix: 상품 단일 조회 쿼리 최적화

--- src/main/java/wayc/backend/shop/domain/command/OptionGroupRepository.java
+++ src/main/java/wayc/backend/shop/domain/command/OptionGroupRepository.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Repository;
 
 import wayc.backend.shop.domain.OptionGroup;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OptionGroupRepository extends JpaRepository<OptionGroup, Long> {
 
     @EntityGraph(attributePaths = "options")
-    @Query("select ogs from OptionGroup ogs where ogs.id =:orgId and ogs.status = 'ACTIVE'")
-    Optional<OptionGroup> findByIdAndStatus(Long orgId);
+    @Query("select ogs from OptionGroup ogs where ogs.item.id = :itemId and ogs.status = 'ACTIVE'")
+    List<OptionGroup> findByItemIdAndStatus(Long itemId);
 }
